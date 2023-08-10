09c8058306ee0c55c29977b95dc0791cfa2202c2
Samuel Eromosele <erosam.se@gmail.com>
2023-08-10 21:45:56 +0000

    Add shebang line

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(0, 99):
-    print('{} is {:#x}'.format(i, i))
+    print('{} = {:#x}'.format(i, i))
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def uppercase(str):
     for s in str:
         if ord(s) >= 97 and ord(s) <= 122:
