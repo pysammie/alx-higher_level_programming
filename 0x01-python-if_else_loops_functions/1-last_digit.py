30dc6c8322bae9c77b9713f965f561dd6ce5e6b6
Samuel Eromosele <erosam.se@gmail.com>
2023-08-10 21:29:00 +0000

    Write python scripts and functions

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,8 +5,10 @@ if number % 10 == 0:
     print(f"Last digit of {number} is {number % 10} and is 0")
 elif number < 0 and number % 10 != 0:
     last_digit = abs(number) % 10
-    print(f"Last digit of {number} is {-last_digit} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {-last_digit} and is less"
+          "than 6 and not 0")
 elif number > 0 and number % 10 > 5:
     print(f"Last digit of {number} is {number % 10} and is greater than 5")
 else:
-    print(f"Last digit of {number} is {number % 10} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {number % 10} and is less"
+          "than 6 and not 0")
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,6 @@
+def print_last_digit(number):
+    if number < 0:
+        number = abs(number)
+    last_digit = number % 10
+    print(f"{last_digit}", end="")
+    return last_digit
