46658e79e8450e6d9d541ef05adfde8959983739
Samuel Eromosele <erosam.se@gmail.com>
2023-08-30 18:44:56 +0000

    Fix pycodestyle error

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,6 +3,6 @@
 def print_reversed_list_integer(my_list=[]):
     """Print all integers of a list in reverse."""
     if isinstance(my_list, list):
-            my_list.reverse()
-            for i in my_list:
-                print("{:d}".format(i))
+        my_list.reverse()
+        for i in my_list:
+            print("{:d}".format(i))
