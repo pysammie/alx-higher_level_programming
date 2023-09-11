752e52f3f76dcc4f93170a68030adec2c5a53acd
Samuel Eromosele <erosam.se@gmail.com>
2023-09-11 23:33:43 +0000

    Class with method not implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Contains class BaseGeometry. """
+
+
+class BaseGeometry():
+    """ A class with method area. """
+
+    def area(self):
+        """ computes area of geometry. """
+
+        raise Exception('area() is not implemented')
