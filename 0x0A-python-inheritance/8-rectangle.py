bb01bf90551a72972248dd348d9e7132f052e8f5
Samuel Eromosele <erosam.se@gmail.com>
2023-09-12 00:24:38 +0000

    Rectangle class

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Contains derived class Rectangle. """
+
+BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Defines class Rectangle. """
+
+    def __init__(self, width, height):
+        """ initializes instance of class Rectangle
+
+        Args:
+            width (int): width of rectangle
+            height (int): height of rectangle
+        """
+
+        self.integer_validator('width', width)
+        self.__width = width
+        self.integer_validator('height', height)
+        self.__height = height
